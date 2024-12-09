5f09c8c3010425d3fb9909d1e77bead11584bec1
mar <arch-legion>
2024-12-09 19:52:55 +0100

    split test-app files

--- rust/test-app/src/main.rs
+++ rust/test-app/src/main.rs
@@ -1,163 +1,15 @@
-use core::{
-    alloc::{self, BufferRequestInfo, ImageRequestInfo},
-    ash::{khr, vk},
-    gpu_allocator::{
-        vulkan::{AllocationScheme, Allocator, AllocatorCreateDesc},
-        MemoryLocation,
-    },
-    winit::{
-        application::ApplicationHandler,
-        dpi::PhysicalSize,
-        event::WindowEvent,
-        event_loop::{ControlFlow, EventLoop},
-        raw_window_handle::{HasDisplayHandle, HasWindowHandle, RawDisplayHandle, RawWindowHandle},
-        window::Window,
-    },
-    Base, Debug, Dev, DevRequestInfo, QueueRequestInfo, Surface, SurfaceRequestInfo, Swapchain,
-    SwapchainRequestInfo,
+use core::winit::{
+    application::ApplicationHandler,
+    dpi::PhysicalSize,
+    event::WindowEvent,
+    event_loop::{ControlFlow, EventLoop},
+    raw_window_handle::{HasDisplayHandle, HasWindowHandle},
+    window::Window,
 };
-use std::{cell::RefCell, error::Error, rc::Rc};
+use std::error::Error;
 
-pub struct Interface {
-    pub allocator: Rc<RefCell<Allocator>>,
-    pub swapchain: Swapchain,
-    pub device: Rc<Dev>,
-    pub surface: Rc<Surface>,
-    pub debug: Option<Debug>,
-    pub base: Base,
-}
-
-impl Interface {
-    pub fn new(
-        display_handle: &RawDisplayHandle,
-        window_handle: &RawWindowHandle,
-    ) -> Result<Self, Box<dyn Error>> {
-        let base = Base::new(vk::API_VERSION_1_3, Some(display_handle))?;
-
-        let debug = if base.debug {
-            Some(Debug::new(&base.entry, &base.instance)?)
-        } else {
-            None
-        };
-
-        let surface = Rc::new(Surface::new(SurfaceRequestInfo {
-            entry: &base.entry,
-            instance: &base.instance,
-            raw_display_handle: display_handle,
-            raw_window_handle: window_handle,
-        })?);
-
-        let mut features_13 = vk::PhysicalDeviceVulkan13Features::default()
-            .dynamic_rendering(true)
-            .synchronization2(true)
-            .maintenance4(true);
-        let mut features_12 = vk::PhysicalDeviceVulkan12Features::default()
-            .descriptor_indexing(true)
-            .buffer_device_address(true);
-        let features = vk::PhysicalDeviceFeatures::default().sampler_anisotropy(true);
-        let features_2 = vk::PhysicalDeviceFeatures2::default()
-            .features(features)
-            .push_next(&mut features_13)
-            .push_next(&mut features_12);
-
-        let device = Rc::new(Dev::new(DevRequestInfo {
-            instance: &base.instance,
-            surface: Some(&surface),
-            queues: vec![QueueRequestInfo {
-                flags: vk::QueueFlags::GRAPHICS,
-                present: true,
-            }],
-            extensions: vec![khr::swapchain::NAME],
-            preferred_type: None,
-            features: None,
-            features_2: Some(features_2),
-        })?);
-
-        let swapchain = Swapchain::new(SwapchainRequestInfo {
-            instance: &base.instance,
-            device: &device,
-            surface: &surface,
-            extent: vk::Extent2D {
-                width: 1280,
-                height: 720,
-            },
-            surface_format: None,
-            present_mode: None,
-        })?;
-
-        let allocator = Rc::new(RefCell::new(
-            Allocator::new(&AllocatorCreateDesc {
-                instance: base.instance.clone(),
-                device: device.dev.clone(),
-                physical_device: device.pdev,
-                debug_settings: Default::default(),
-                buffer_device_address: true,
-                allocation_sizes: Default::default(),
-            })
-            .unwrap(),
-        ));
-
-        Ok(Self {
-            allocator,
-            swapchain,
-            device,
-            surface,
-            debug,
-            base,
-        })
-    }
-
-    fn alloc_test(&mut self) {
-        {
-            let _ = alloc::Buffer::new(&BufferRequestInfo {
-                allocator: &self.allocator,
-                device: &self.device,
-                linear: true,
-                location: MemoryLocation::CpuToGpu,
-                dedicated: true,
-                info: &vk::BufferCreateInfo::default()
-                    .size(512)
-                    .usage(vk::BufferUsageFlags::TRANSFER_SRC),
-            })
-            .unwrap();
-        }
-        let _ = alloc::Image::new(&ImageRequestInfo {
-            allocator: &self.allocator,
-            device: &self.device,
-            linear: false,
-            location: MemoryLocation::GpuOnly,
-            dedicated: true,
-            img_info: &vk::ImageCreateInfo::default()
-                .image_type(vk::ImageType::TYPE_2D)
-                .format(vk::Format::R16G16B16A16_SFLOAT)
-                .extent(vk::Extent3D {
-                    width: self.swapchain.extent.width,
-                    height: self.swapchain.extent.height,
-                    depth: 1,
-                })
-                .mip_levels(1)
-                .array_layers(1)
-                .samples(vk::SampleCountFlags::TYPE_1)
-                .tiling(vk::ImageTiling::OPTIMAL)
-                .usage(
-                    vk::ImageUsageFlags::COLOR_ATTACHMENT
-                        | vk::ImageUsageFlags::STORAGE
-                        | vk::ImageUsageFlags::TRANSFER_SRC
-                        | vk::ImageUsageFlags::TRANSFER_DST,
-                ),
-            img_view_info: &vk::ImageViewCreateInfo::default()
-                .view_type(vk::ImageViewType::TYPE_2D)
-                .format(vk::Format::R16G16B16A16_SFLOAT)
-                .subresource_range(
-                    vk::ImageSubresourceRange::default()
-                        .aspect_mask(vk::ImageAspectFlags::COLOR)
-                        .level_count(1)
-                        .layer_count(1),
-                ),
-        })
-        .unwrap();
-    }
-}
+mod interface;
+use interface::Interface;
 
 #[derive(Default)]
 struct TestApp {
@@ -191,8 +43,6 @@ impl ApplicationHandler for TestApp {
 
         interface.alloc_test();
 
-        event_loop.exit();
-
         window.set_visible(true);
         self.window = Some(window);
         self.vk = Some(interface);
@@ -210,8 +60,7 @@ impl ApplicationHandler for TestApp {
                 self.vk
                     .as_mut()
                     .unwrap()
-                    .swapchain
-                    .recreate(size.width, size.height)
+                    .resize(size.width, size.height)
                     .unwrap();
             }
             WindowEvent::CloseRequested => event_loop.exit(),
